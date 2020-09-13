67474024916b2b1aa83fc0e01dff596ac6b7f6e1
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-09-13 23:46:18 +0200

    added a bit of info from Josefine

--- src/Components/ContactForm.js
+++ src/Components/ContactForm.js
@@ -7,6 +7,7 @@ import { useForm } from "react-hook-form";
 
 // Styles
 import "react-toastify/dist/ReactToastify.css";
+import { Card } from "react-bootstrap";
 
 export default function ContactForm() {
   const { register, handleSubmit, errors } = useForm();
@@ -16,61 +17,74 @@ export default function ContactForm() {
   };
 
   return (
-    <div className='card card-default col-lg-3 col-md-5 col-sm-6 mt-5 p-3 float-right contact-form'>
-      <Form onSubmit={handleSubmit(onSubmit)} id='contact-form'>
-        <Form.Row>
-          <Form.Label className='text-success'>
-            Brug denne kontakt formular, og så vender jeg tilbage hurtigst
-            muligt :)
-          </Form.Label>
-        </Form.Row>
-        <Form.Row>
-          <Form.Control
-            tabIndex='1'
-            name='name'
-            required
-            placeholder='Indtast Navn'
-            ref={register({ required: true })}
-          />
-        </Form.Row>
-        <Form.Row className='mt-3'>
-          <Form.Control
-            tabIndex='2'
-            name='email'
-            id='email'
-            placeholder='Indtast Email'
-            required
-            style={{
-              border: errors.email && "2px solid red",
-            }}
-            ref={register({
-              required: true,
-              pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-            })}
-          />
-        </Form.Row>
-        <Form.Row className='mt-3'>
-          <Form.Control
-            tabIndex='3'
-            name='message'
-            as='textarea'
-            required
-            rows='8'
-            placeholder='Skriv en besked'
-            ref={register({ required: true })}
-          />
-        </Form.Row>
+    <div>
+      <Card
+        variant='dark'
+        className='mt-5 mx-1 col-10 col-sm-10 col-md-10 col-lg-6 col-xl-4 p-3 float-left'
+      >
+        Hvis du har spørgsmål eller vil tage en snak om en eventuel fotosession
+        hos Grobund Fotografi, så skriv her. Jeg vender tilbage til dig hurtigst
+        muligt!{" "}
+        <span role='img' aria-label='Smile Emoji'>
+          😊
+        </span>
+      </Card>
 
-        <Button
-          tabIndex='4'
-          variant='outline-success'
-          className='mt-3'
-          type='submit'
-        >
-          Send
-        </Button>
-      </Form>
-      <ToastContainer />
+      <div className='card card-default col-10 col-sm-6 col-md-6 col-lg-4 col-xl-3 mt-5 p-3 mx-1 contact-form float-right'>
+        <Form onSubmit={handleSubmit(onSubmit)} id='contact-form'>
+          <Form.Row>
+            <Form.Label className='text-success'>
+              Hvad kan jeg gøre for dig?
+            </Form.Label>
+          </Form.Row>
+          <Form.Row>
+            <Form.Control
+              tabIndex='1'
+              name='name'
+              required
+              placeholder='Indtast Navn'
+              ref={register({ required: true })}
+            />
+          </Form.Row>
+          <Form.Row className='mt-3'>
+            <Form.Control
+              tabIndex='2'
+              name='email'
+              id='email'
+              placeholder='Indtast Email'
+              required
+              style={{
+                border: errors.email && "2px solid red",
+              }}
+              ref={register({
+                required: true,
+                pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              })}
+            />
+          </Form.Row>
+          <Form.Row className='mt-3'>
+            <Form.Control
+              tabIndex='3'
+              name='message'
+              as='textarea'
+              required
+              rows='8'
+              placeholder='Skriv en besked'
+              ref={register({ required: true })}
+            />
+          </Form.Row>
+
+          <Button
+            tabIndex='4'
+            variant='outline-success'
+            className='mt-3'
+            type='submit'
+          >
+            Send
+          </Button>
+        </Form>
+        <ToastContainer />
+      </div>
     </div>
   );
 }
--- src/Routes/om-grobund/About.js
+++ src/Routes/om-grobund/About.js
@@ -2,9 +2,57 @@ import React from "react";
 
 const About = () => {
   return (
-    <header className='App-header'>
-      <h1>Om Grobund - Kommer snart</h1>
-    </header>
+    <div className='text-white p-3'>
+      <h2>Om Grobund</h2>
+      <hr className='bg-white'></hr>
+      <p>
+        Mit navn er Josefine, jeg er mor, kone, naturelsker og derudover
+        indehaver af Grobund Fotografi.
+      </p>
+      <p>
+        Jeg har elsket at tage billeder og dokumentere alt via denne form, siden
+        jeg var lille.
+      </p>
+      Siden 2018 har jeg givet denne elskede hobby mere plads i mit liv, og
+      meget hurtigt optog den meget tid, da faget er noget der kaldte på mig.
+      <p>I 2019 blev Grobund Fotografi til.</p>
+      <p>
+        Den kreative udtryksform er noget der har skabt genklang igennem hele
+        mit liv, men jeg har derudover erfaring indenfor pædagogfaget og jeg ser
+        mit moderskab som et af mine største livsrejser og en kæmpe del af mit
+        liv.
+      </p>
+      <h3>Livsrejser</h3> <hr className='bg-white'></hr>
+      <p>
+        Når jeg tager billeder, handler det for mig om at skabe ærlige minder.
+        Jeg vil fange de oprigtige smil og det livsstadie jeg fotografere.
+        Uanset om det er en baby der ikke kan gå eller et brudepar, så vil jeg
+        gøre mit ypperste for at fange den stemning og det ærlige øjeblik jeg
+        ser. Det handler om at vi hver i sær er på en livsrejse, og går man til
+        en fotograf, så er det for at dokumentere en del af den rejse. Det skal
+        gøres ærligt.
+      </p>
+      <h3>Naturen</h3> <hr className='bg-white'></hr>
+      <p>
+        Jeg føler mig forbundet og tiltrukket af naturen, derfor blev dette også
+        min første muse. Især makro fotografi har fået et greb om mit hjerte.
+        Ved en fotosession hos mig, bruger vi også naturen. Både som legeplads
+        og inspiration. Alt foregår udendørs, med naturligt lys og det vejr vi
+        nu bliver skænket på dagen. Dette er også med til at fange det
+        specifikke øjeblik vi befinder os i, og gør billederne autentiske.
+      </p>
+      <h3>Hvorfor vælge Grobund Fotografi</h3> <hr className='bg-white'></hr>
+      <p>
+        Hos Grobund Fotografi får du ærlighed, naturlige - ikke opstillede
+        billeder i høj kvalitet til en god pris. Der er mulighed for
+        korrespondance gennem hele forløbet, og kontakten mellem kunde og mig
+        selv, er noget jeg vægter ekstremt højt. Vi skal kunne stole på
+        hinanden, for at alle får en god oplevelse. Børnefotografering foregår
+        altid på børnenes præmisser og jeg sørger altid for at skabe en relation
+        til barnet, inden selve fotograferingen går i gang. Denne tid er ekstrem
+        vigtig for mig.
+      </p>
+    </div>
   );
 };
 

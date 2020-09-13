936eb8a93afafb6980adeee1cf460abe91a55c45
Niels Pilgaard Grøndahl <niels.pilgaard@hotmail.com>
2020-09-13 23:17:47 +0200

    Removed mail & postal code from bottom nav

--- src/App.js
+++ src/App.js
@@ -13,22 +13,14 @@ function App() {
         <Navigation />
       </BrowserRouter>
       ,
-      <Navbar className='navbar-bottom' fixed='bottom'>
-        <div className='d-inline-flex mt-3 mx-auto'>
+      <Navbar className='navbar-bottom' sticky='bottom'>
+        <div className='d-inline-flex mx-auto mt-3'>
           <p>
             <span role='img' aria-label='Copyright' className='mr-1'>
               ©
             </span>
             Grobund Fotografi 2020
           </p>
-
-          <a
-            className='text-warning ml-5'
-            href='mailto:mail@grobundfotografi.dk'
-          >
-            mail@grobundfotografi.dk
-          </a>
-          <p className='ml-5'>8550 Ryomgård</p>
         </div>
       </Navbar>
     </div>
